e8377cfc110dd51f186594e4980b4362a4d95a21
Nate J.M <nate@natejonah.com>
2023-02-19 20:34:28 +0000

    Create destinations JS file with all data

---
+++ src/destinations.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: 1,
+        img: "https://source.unsplash.com/L6EP96_w1Fc",
+        country: "South Africa",
+        mapLink: "https://goo.gl/maps/BS8ZVjbx5Vtm1bDz6 ",
+        destination: "Table Mountain",
+        dates: "circa 1996-1998",
+        description: "Table Mountain is a flat-topped mountain forming a prominent landmark overlooking the city of Cape Town in South Africa. It is a significant tourist attraction, with many visitors using the cableway or hiking to the top."
+    },
+    {
+        id: 2,
+        img: "https://source.unsplash.com/bO3anYDHmFk",
+        country: "United Kingdom",
+        mapLink: "https://goo.gl/maps/ZNLixpNUneLDawL9A",
+        destination: "Regent's Park",
+        dates: "21 Sep, 2015",
+        description: "Regent's Park is one of the Royal Parks of London. It occupies 410 acres of high ground in north-west Inner London, administratively split between the City of Westminster and the Borough of Camden."
+    },
+    {
+        id: 3,
+        img: "https://source.unsplash.com/946XeVrYO2E",
+        country: "Netherlands",
+        mapLink: "https://goo.gl/maps/MpvmFZ4btE9yfsbUA",
+        destination: "Erasmusbrug",
+        dates: "28 Apr, 2017 - 03 May, 2017",
+        description: "The Erasmusbrug is a combined cable-stayed and bascule bridge, construction began in 1986 and was completed in 1996. It crosses the Nieuwe Maas in the centre of Rotterdam, connecting the north and south parts of this city, second largest in the Netherlands."
+    }
+]
