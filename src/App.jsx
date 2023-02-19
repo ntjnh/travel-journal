b69f643fe5da0b9adaa682fc761e94bf86045a90
Nate J.M <nate@natejonah.com>
2023-02-19 21:11:35 +0000

    Display data in Card instance in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import './App.scss'
 import Card from './assets/components/Card'
 import Header from './assets/components/Header'
+import destinations from './destinations'
 
 function App() {
+  const places = destinations.map(place => {
+    return (
+      <Card
+        key={place.id}
+        {...place}
+      />
+    )
+  })
 
   return (
     <div className="App">
       <Header />
       <main className="main">
-        <Card />
+        {places}
       </main>
     </div>
   )
